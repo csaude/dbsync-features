d321d7b965f11f30fbc6605ae3775e11b75f6d8d
JPboane <jpboane@gmail.com>
2023-12-11 12:06:59 +0200

    Make manager-app works

---
+++ manager-app/src/main/java/org/mz/csaude/dbsyncfeatures/manager/app/DbsyncManagerApplication.java
@@ -0,0 +1,13 @@
+package org.mz.csaude.dbsyncfeatures.manager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbsyncManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbsyncManagerApplication.class, args);
+	}
+
+}
